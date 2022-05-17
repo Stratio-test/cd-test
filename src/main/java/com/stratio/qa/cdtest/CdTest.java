d4b6fc27f5ba379aec6f2440193c6b3e0315b12c
lpgutierrez <92737780+lpgutierrez@users.noreply.github.com>
2022-05-17 15:16:01 +0200

    Update CdTest.java

--- src/main/java/com/stratio/qa/cdtest/CdTest.java
+++ src/main/java/com/stratio/qa/cdtest/CdTest.java
@@ -3,5 +3,9 @@ package com.stratio.qa.cdtest;
 public class CdTest {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
+        String name = "test";
+        if (name == "technical"){
+            System.out.println("Into a if!!");
+        }
     }
 }
