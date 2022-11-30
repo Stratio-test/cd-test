fb0d751801d972d7fccbf0cc9bbbfe98407fbc27
lpgutierrez <92737780+lpgutierrez@users.noreply.github.com>
2022-11-30 13:46:52 +0100

    Update test.py

--- python/test.py
+++ python/test.py
@@ -1,7 +1,7 @@
 import os
 print("Hello world Laura!")
-cred1 = os.environ["VAR1"]
+cred1 = os.environ["CREDENTIALS_VARIABLE"]
 print(cred1)
-cred2 = os.environ["VAR2"]
-print(cred2)
+# cred2 = os.environ["VAR2"]
+# print(cred2)
 
