c3cd39c60976b3652b84cc061ee3dd10c3cd9af9
lpgutierrez <92737780+lpgutierrez@users.noreply.github.com>
2022-12-20 17:19:25 +0100

    Create deploy.sh

---
+++ bin/deploy.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash -e
+
+DIR=dist
+BASEDIR="$(realpath "$(dirname "$0")/..")"
+
+if [[ -z "$1" ]]; then
+	VERSION=$(cat $BASEDIR/VERSION)
+else
+	VERSION=$1
+fi
+
+cd $BASEDIR
+
+echo "Uploading to Nexus kms_utils.sh version $VERSION"
+curl -u stratio:${NEXUSPASS} --upload-file bundle.json http://qa-pre.int.stratio.com/repository/paas/testmake/${VERSION}/bundle.json
